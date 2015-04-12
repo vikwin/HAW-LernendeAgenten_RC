c63b64abaf03f0769c2d820bccdfce65e15a9507
Alexander Oster <alexander.oster@haw-hamburg.de>
2015-04-12 22:14:03 +0200

    Bot und Environment etwas erweitert.

--- LARCbot_DanielAlex/LARC/src/environment/LARCEnvironment.java
+++ LARCbot_DanielAlex/LARC/src/environment/LARCEnvironment.java
@@ -9,29 +9,22 @@ import robot.LARCRobot;
 
 public class LARCEnvironment implements EnvironmentInterface {
 
+	public static final int tileSize = 40;
+
 	public boolean[][] grid;
 	public LARCRobot robot;
+	WorldDescription theWorld;
 
 	public LARCEnvironment(LARCRobot robot) {
 		this.robot = robot;
 	}
 
 	@Override
-	public void env_cleanup() {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
 	public String env_init() {
-		this.grid = new boolean[(int) (robot.getBattleFieldWidth() / robot
-				.getWidth())][(int) (robot.getBattleFieldHeight() / robot
-				.getBattleFieldHeight())];
-		return null;
-	}
+		this.grid = new boolean[(int) (robot.getBattleFieldWidth() / tileSize)][(int) (robot
+				.getBattleFieldHeight() / tileSize)];
 
-	@Override
-	public String env_message(String arg0) {
-		// TODO Auto-generated method stub
+		theWorld = new WorldDescription(grid,1,1);
 		return null;
 	}
 
@@ -47,4 +40,37 @@ public class LARCEnvironment implements EnvironmentInterface {
 		return null;
 	}
 
+	@Override
+	public String env_message(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void env_cleanup() {
+		// TODO Auto-generated method stub
+	}
+
+	class WorldDescription {
+		boolean[][] grid;
+		int startRow;
+		int startCol;
+		int numRows;
+		int numCols;
+
+		public WorldDescription(boolean[][] grid, int startRow, int startCol) {
+			this.grid = grid;
+			this.startRow = startRow;
+			this.startCol = startCol;
+			this.numRows = grid.length;
+			this.numCols = grid[0].length;
+		}
+
+		public int getNumStates() {
+			return numRows * numCols;
+		}
+
+		public void updatePosion() {
+		}
+	}
 }
